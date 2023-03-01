dbcbf11640bfc7a91fcace024b4d645a01cff33d
vasilring <vasilliring@gmail.com>
2023-03-02 00:55:25 +0200

    Add project files.

---
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,29 @@
+﻿using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //-- Arrange
+
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+            string expected = "Baggins, Bilbo";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
