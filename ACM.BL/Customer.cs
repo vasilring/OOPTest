d5c2e10c814689ef7c17b79185b8d4ce6ffb9eec
vasilring <vasilliring@gmail.com>
2023-03-02 16:20:55 +0200

    new changes of unit test

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -19,22 +19,36 @@ namespace ACM.BL
         {
             get
             {
-                return LastName + ", " + FirstName;
+                string fullName = LastName;// if there is last name defined, the full name is the lastname with no commas
+                if(!string.IsNullOrWhiteSpace(FirstName))//if there is only a firstname, the fullname is initially null, follow p2--
+                {
+                    if(!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";// if there is both the firstname and the lastname, we start with the last name, add a coma and a space before appending the firstname
+                    }
+                    fullName += FirstName; // p2.-- so we set the fullname to the firstname
+                }
+                return fullName;
             }
         }
 
         private string _lastName;
 
-        public string LastName 
-        { 
-            get 
-            { 
+        //public string LastName { get; set; }
+
+        // Горното или долното, и двете извършват една и съща функция в момента,
+        // ако нямаме допълнителна логика в долното, може да използваме горното
+
+        public string LastName
+        {
+            get
+            {
                 return _lastName;
-            } 
-        
-            set 
-            { 
-                _lastName = value; 
+            }
+
+            set
+            {
+                _lastName = value;
             }
         }
     }
--- Tests/ACM.BLTest/CustomerTest.cs
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -25,5 +25,41 @@ namespace ACM.BLTest
             //-- Assert
             Assert.AreEqual(expected, actual);
         }
+       
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //-- Arrange
+
+            Customer customer = new Customer
+            {
+                LastName = "Baggins"
+            };
+            string expected = "Baggins";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //-- Arrange
+
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo"
+            };
+            string expected = "Bilbo";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
